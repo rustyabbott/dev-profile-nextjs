236688633452180fe337511909aee0431f6f7e78
Rusty Abbott <dev@rustyabbott.com>
2024-05-04 01:58:55 -0400

    Add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: "Rusty Abbott | Developer Profile",
@@ -13,6 +15,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="img/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </Head>
       <body className="bg-black">{children}</body>
     </html>
   );
