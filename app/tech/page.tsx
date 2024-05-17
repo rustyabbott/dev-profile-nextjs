0851f5d378f5f6f83838042f6551104f40e09ca8
Rusty Abbott <dev@rustyabbott.com>
2024-05-17 07:32:31 -0400

    Convert CSS to Tailwind utility classes - headers, main

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -10,15 +10,15 @@ export default function Tech() {
 
       <main className="pageWrapper">
 
-        <h1>&lt;Technology /&gt;</h1>
+        <h1 className="text-right font-extrabold my-2.5 text-2xl">&lt;Technology /&gt;</h1>
 
-        <h2>Software, Languages, &amp; Frameworks</h2>
+        <h2 className="text-xl font-bold mb-1">Software, Languages, &amp; Frameworks</h2>
 
         <p>
           Proficieny: PHP, JavaScript, TypeScript, MySQL, Laravel, React, Vue, Symfony, Tailwind, CSS, Linux (Ubuntu), Bash, AWS services (EC2, ASG, CloudWatch, S3, SQS), CI/CD via GitLab & GitHub. Familiarity: Python, Django, Go, Kubernetes, PyTorch.
         </p>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           National Wild Turkey Federation (<Link href="https://www.nwtf.org/" target="_blank" rel="noopener noreferrer">Company Site</Link>)
         </h2>
 
@@ -40,7 +40,7 @@ export default function Tech() {
           I&apos;m currently a full stack developer at the National Wild Turkey Federation. My primary project is re-writing their events management system with Laravel Sail + Inertia.js (Vue). I also maintain and update the existing system, plus lead Devops using GitLab CI/CD and AWS services.
         </p>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           SureSwift Capital (<Link href="https://www.sureswiftcapital.com" target="_blank" rel="noopener noreferrer">Company Site</Link>)
         </h2>
 
@@ -74,7 +74,7 @@ export default function Tech() {
           </Link>
         </div>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           REP Fitness (<Link href="https://www.repfitness.com" target="_blank" rel="noopener noreferrer">eCommerce Site</Link>)
         </h2>
 
@@ -95,7 +95,7 @@ export default function Tech() {
           Prior to SureSwift Capital, I was a Solutions Developer for REP Fitness in Denver, Colorado. Responsibilities included automating processes and maximizing procedural efficiency. Tech stack: PHP, MySQL, Magento 2, jQuery, Git, webhooks, and multiple REST API integrations (Kuebix, Brightpearl, ShipStation, UPS, and more).
         </p>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           InMotion Hosting (<Link href="https://www.inmotionhosting.com" target="_blank" rel="noopener noreferrer">Marketing Site</Link>)
         </h2>
 
@@ -116,7 +116,7 @@ export default function Tech() {
           Prior to REP Fitness, I was a developer for InMotion Hosting, working on multiple marketing sites and blogs, including <Link href="https://www.inmotionhosting.com" target="_blank" rel="noopener noreferrer">inmotionhosting.com</Link> and <Link href="https://www.webhostinghub.com" target="_blank" rel="noopener noreferrer">webhostinghub.com</Link>. Technologies included Symfony 2 & 4, PHP, GitLab CI/CD, jQuery/Javascript, Sass, Nginx, MariaDB, WordPress, and CentOS.
         </p>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           RustyAbbott.com (<Link href="https://github.com/rustyabbott/rustyabbott.com" target="_blank" rel="noopener noreferrer">GitHub</Link>)
         </h2>
 
@@ -124,7 +124,7 @@ export default function Tech() {
           The web app you&apos;re currently viewing was originally built with Facebook&apos;s <Link href="https://github.com/facebook/create-react-app" target="_blank" rel="noopener noreferrer">Create React App</Link>. Recently I&apos;ve migrated to Next.js and use GitHub Actions to deploy to an AWS EC2 instance.
         </p>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           Memory Game (<Link href="https://github.com/rustyabbott/memory-game" target="_blank" rel="noopener noreferrer">GitHub</Link>, <Link href="https://rustyabbott.github.io/memory-game/" target="_blank" rel="noopener noreferrer">Play Live</Link>)
         </h2>
 
@@ -144,7 +144,7 @@ export default function Tech() {
             />
         </div>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           Arcade Game (<Link href="https://github.com/rustyabbott/arcade-game" target="_blank" rel="noopener noreferrer">GitHub</Link>, <Link href="https://rustyabbott.github.io/arcade-game/" target="_blank" rel="noopener noreferrer">Play Live</Link>)
           </h2>
         <p>
@@ -163,7 +163,7 @@ export default function Tech() {
             />
         </div>
 
-        <h2>
+        <h2 className="text-xl font-bold mb-1">
           Analog Clock (<Link href="https://github.com/rustyabbott/clock" target="_blank" rel="noopener noreferrer">GitHub</Link>, <Link href="https://rustyabbott.github.io/clock/" target="_blank" rel="noopener noreferrer">Play Live</Link>)
         </h2>
 
