af4fffb226e54cf6b57b996fb7694d8cf02cdb54
Rusty Abbott <dev@rustyabbott.com>
2024-05-19 14:48:47 -0400

    Switch to client components, identify active pages in header

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,7 +1,14 @@
+"use client";
+
 import Link from 'next/link';
 import Image from 'next/image';
+import { usePathname } from 'next/navigation';
 
 export default function Header() {
+    const pathname = usePathname();
+    const isTechPage = pathname === '/tech';
+    const isHomePage = pathname === '/';
+
     return (
         <header className="bg-[#0b6cfd] w-full text-white h-14 fixed top-0">
             <div className="max-w-5xl m-auto text-2xl h-full">
@@ -14,8 +21,8 @@ export default function Header() {
                     </Link>
                 </nav>
                 <div className="hidden sm:flex sm:items-center sm:justify-between sm:h-full sm:px-4">
-                    <Link href="/">Home</Link>
-                    <Link href="/tech">Technology</Link>
+                    <Link href="/" className={`hover:text-black ${isHomePage ? 'underline' : ''}`}>Home</Link>
+                    <Link href="/tech" className={`hover:text-black ${isTechPage ? 'underline' : ''}`}>Technology</Link>
                 </div>
             </div>
         </header>
